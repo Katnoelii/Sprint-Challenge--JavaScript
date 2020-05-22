99cc6dd7923183920c847ec4245c3ae845c69292
kate roy <kroy15chs@gmail.com>
2020-05-22 12:20:27 -0400

    on counter task of closure

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,14 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+//nestedFunction can access the variable internal because it inherits its parent's variables. Any nested function has access to the variables that belong to the functions they are nested in.
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function sumation(num1){
+  for( i = 0; i<=num1; i++){
+    return [i];
+  }
+}
+console.log(sumation(4));
